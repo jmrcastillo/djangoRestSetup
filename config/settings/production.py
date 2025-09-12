f648e42dcc536d2ddd3ce2823d842e476dabec85
dm <jmrcas09@gmail.com>
2025-09-12 14:38:25 +0800

    Production - Production setup

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,21 +2,48 @@
 
 from .base import *
 from decouple import config
+import os
+import environ
 
 DEBUG = False
 
+SECRET_KEY = os.environ.get("SECRET_KEY", "unsafe-dev-key")
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+
 ALLOWED_HOSTS = ['yourdomain.com']
 
-# DATABASES = {
-    # 'default': {
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': config('POSTGRES_DB'),
-        # 'USER': config('POSTGRES_USER'),
-        # 'PASSWORD': config('POSTGRES_PASSWORD'),
-        # 'HOST': config('POSTGRES_HOST', 'localhost'),
-        # 'PORT': config('POSTGRES_PORT', '5432'),
-    # }
-# }
+# SSL & CSRF
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+CSRF_TRUSTED_ORIGINS = [
+    "https://upland-api-production-001e.up.railway.app",
+    "https://upland-frontend-nu.vercel.app"
+]
+
+# Allow your Vercel frontend
+CORS_ALLOWED_ORIGINS = [
+    "https://upland-frontend-git-main-jmrcas-projects.vercel.app",
+    "https://upland-frontend-nu.vercel.app",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+# Postgresql & Env Variable
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('PGDATABASE'),
+        'USER': env('PGUSER'),
+        'PASSWORD': env('PGPASSWORD'),
+        'HOST': env('PGHOST'),
+        'PORT': env('PGPORT'),
+    }
+}
 
 # Add production-specific settings like security headers, etc.
 
