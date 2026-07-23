bf3108c72f579f7a8ffd5925a8a467a37a7a94c8
dm <jmrcas09@gmail.com>
2026-07-23 13:49:28 +0800

    Accounts - create and seperate account details

---
+++ accounts/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('', views.getRoutes),
+    path('auth/register', views.register_view, name='register'),
+    path('auth/logout', views.logout_view, name='logout'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+]
+
--- config/settings/base.py
+++ config/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
     #Local
+    'accounts',
 ]
 
 REST_FRAMEWORK = {
