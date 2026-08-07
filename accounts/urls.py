ba56824142a7081a768ce8275df4bf9a58d13615
dm <jmrcas09@gmail.com>
2026-08-08 05:54:36 +0800

    accounts: add login and register endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,8 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('', views.getRoutes),
+    path('auth/login/', views.login_view, name='login'),
+    path('auth/refresh/', views.refresh_view, name='refresh'),
     path('auth/register/', views.register_view, name='register'),
     path('auth/logout/', views.logout_view, name='logout'),
     path('auth/me/', views.me_view),
