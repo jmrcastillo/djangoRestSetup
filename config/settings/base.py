5f209b67b0d2a41b4a2d103346bcb7348789406b
dm <jmrcas09@gmail.com>
2026-08-07 13:22:20 +0800

    jmrcas app: Add API Documentation

--- config/settings/base.py
+++ config/settings/base.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'corsheaders',
+    'drf_spectacular',
     #Local
     'accounts',
 ]
@@ -88,7 +89,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
 SIMPLE_JWT = {
